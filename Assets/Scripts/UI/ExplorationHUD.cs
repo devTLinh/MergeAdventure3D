17dcd98bf3c2dc3e7f1322340e421d4988970e35
devTLinh <trinhlinh04.cv@gmail.com>
2026-05-24 01:29:35 +0700

    commit UI EnergyRegenManager

--- Assets/Scripts/UI/ExplorationHUD.cs
+++ Assets/Scripts/UI/ExplorationHUD.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 public class ExplorationHUD : MonoBehaviour { 
     [SerializeField] Text text;
+    [SerializeField] Text exploreTimerText;
     [SerializeField] Slider slider;
     void Start()
     {
@@ -11,5 +13,12 @@ public class ExplorationHUD : MonoBehaviour {
     {
         text.text = ExplorationEnergyManager.Instance.CurrentEnergy.ToString();
         slider.value = ExplorationEnergyManager.Instance.CurrentEnergy > ExplorationEnergyManager.Instance.MaxEnergy ? 1 : (float)ExplorationEnergyManager.Instance.CurrentEnergy / ExplorationEnergyManager.Instance.MaxEnergy;
+        if (ExplorationEnergyManager.Instance.CurrentEnergy >= ExplorationEnergyManager.Instance.MaxEnergy)
+        {
+            exploreTimerText.text = "FULL";
+            return;
+        }
+        TimeSpan remain = EnergyRegenManager.Instance.ExploreCountdown();
+        exploreTimerText.text = string.Format("{0:00}:{1:00}", remain.Minutes, remain.Seconds);
     }
 }
--- Assets/Scripts/UI/HUDController.cs
+++ Assets/Scripts/UI/HUDController.cs
@@ -1,7 +1,9 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 public class HUDController : MonoBehaviour {
     [SerializeField] Text energyText;
+    [SerializeField] Text energyTimerText;
     [SerializeField] Slider energySlider;
     void Start()
     {
@@ -10,5 +12,12 @@ public class HUDController : MonoBehaviour {
     void Update() {
         energyText.text = EnergyManager.Instance.current.ToString();
         energySlider.value = EnergyManager.Instance.current > EnergyManager.Instance.max ? 1 : (float)EnergyManager.Instance.current / EnergyManager.Instance.max;
+        if (EnergyManager.Instance.current >= EnergyManager.Instance.max)
+        {
+            energyTimerText.text = "FULL";
+            return;
+        }
+        TimeSpan remain =EnergyRegenManager.Instance.EnergyCountdown();
+        energyTimerText.text = string.Format("{0:00}:{1:00}", remain.Minutes, remain.Seconds);
     }
 }
